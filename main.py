dfc8d8f27bc19e2cdf387dd5f0987806f529923e
aminat ajagbe <ajagbeaminat95@gmail.com>
2020-12-17 10:43:46 +0000

    Remove secret

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import requests
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 OWM_ENDPOINT = "https://api.openweathermap.org/data/2.5/onecall"
-API_KEY = "8e41d0067ebf2047856f1346b0557899"
+
 
 weather_params = {
     "lat": 53.419790,
     "lon": -6.395090,
-    "appid": API_KEY,
+    "appid": os.getenv("API_KEY"),
+    "exclude": "current,minutely,daily",
 }
-
 response = requests.get(OWM_ENDPOINT, params=weather_params)
 response.raise_for_status()
 data = response.json()
